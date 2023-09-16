d872f28264cf72d8515fb12e5cdceea7abbe09a0
Halil Gedik <halilgedik4234@gmail.com>
2023-09-16 14:29:35 +0300

    What is Model Binding? ModelBinding with TagHelpers

--- AspDotnetCoreWorkshop/Controllers/WorkshopController.cs
+++ AspDotnetCoreWorkshop/Controllers/WorkshopController.cs
@@ -5,14 +5,15 @@ namespace AspDotnetCoreWorkshop.Controllers
 {
     public class WorkshopController : Controller
     {
+        // !tuple nesnesi ile veri gönderimi
         public IActionResult Index()
         {
 
             Product product = new()
             {
-                Id = 1,
                 Name = "Iphone 15",
-                Quantity = 5
+                Quantity = 5,
+                Price = 15000
             };
 
             User user = new()
@@ -25,6 +26,39 @@ namespace AspDotnetCoreWorkshop.Controllers
             (Product products, User users) userProductTuple = (product, user);
 
             return View(userProductTuple);
+        }
+
+        // ! Model Binding
+        public IActionResult CreateProduct()
+        {
+            Product product = new()
+            {
+                Name = "Xiaomi mi 15",
+                Quantity = 5,
+                Price = 20000
+            };
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(Product p)
+        {
+            if (p != null)
+            {
+
+                Product product = new()
+                {
+                    Name = p.Name,
+                    Quantity = p.Quantity,
+                    Price = p.Price
+                };
+
+            }
+
+
+
+            return RedirectToAction("CreateProduct");
         }
     }
 }
